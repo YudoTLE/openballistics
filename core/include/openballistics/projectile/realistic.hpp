c3c48c3d4e53f2db0d2ff0475d0c0f07821cc8e1
YudoTLE <yudojago@gmail.com>
2026-05-04 10:24:15 +0700

    feat: m56a3 projectile preset

--- core/include/openballistics/projectile/realistic.hpp
+++ core/include/openballistics/projectile/realistic.hpp
@@ -13,17 +13,49 @@
 
 namespace openballistics::projectile
 {
-    class realistic
-    {
+	class realistic
+	{
+	public:
+		[[nodiscard]] static realistic m56a3()
+		{
+			constexpr double mass = 0.10141;									   // kg    (101.41 gm)
+			constexpr double diameter = 0.02;									   // m     (20mm)
+			constexpr double referenceArea = 3.14159265358979323846 * 0.01 * 0.01; // m²    (π·r², r = 10mm)
+			constexpr double axialMomentOfInertia = 5.478e-6;					   // kg·m² (54.78 gm·cm²)
+
+			return realistic{}
+				.set_mass(mass)
+				.set_diameter(diameter)
+				.set_reference_area(referenceArea)
+				.set_axial_moment_of_inertia(axialMomentOfInertia)
+				.set_drag_force_coefficient(
+					{0.000, 0.620, 0.780, 0.870, 0.930, 0.950,
+					 1.020, 1.060, 1.100, 1.200, 1.310, 1.380,
+					 1.530, 1.700, 2.030, 2.475, 3.000, 3.500},
+					{0.2800, 0.2756, 0.2700, 0.2470, 0.2860, 0.3090,
+					 0.5260, 0.5590, 0.5680, 0.5760, 0.5700, 0.5610,
+					 0.5280, 0.5080, 0.4830, 0.4280, 0.3860, 0.3570})
+				.set_lift_force_coefficient(
+					{0.0, 0.9, 1.1, 1.3, 1.6, 2.0, 2.4, 3.4, 3.5},
+					{1.944, 1.944, 1.923, 2.100, 2.311, 2.484, 2.554, 2.317, 2.283})
+				.set_overturning_moment_coefficient(
+					{0.00, 0.40, 0.60, 0.71, 0.85, 0.96,
+					 1.06, 1.15, 2.20, 2.70, 3.10, 3.30, 3.50},
+					{2.48, 2.48, 2.54, 2.60, 2.76, 3.07,
+					 2.80, 2.72, 2.16, 1.93, 1.80, 1.77, 1.75})
+				.set_spin_damping_moment_coefficient(0.013)
+				.set_magnus_force_coefficient(0.0);
+		}
+
 	public:
 		realistic()
-        {
+		{
 #pragma region "CODEGEN PROJ-R-DEF" // AUTO-GENERATED - DO NOT EDIT MANUALLY
 
 #pragma endregion "CODEGEN PROJ-R-DEF"
-        }
+		}
 
-    public:
+	public:
 #pragma region "CODEGEN PROJ-R-SET" // AUTO-GENERATED - DO NOT EDIT MANUALLY
 		realistic &set_mass(const scalar value)
 		{
@@ -310,7 +342,7 @@ namespace openballistics::projectile
 		}
 #pragma endregion "CODEGEN PROJ-R-GET"
 
-    private:
+	private:
 #pragma region "CODEGEN PROJ-R-MEM" // AUTO-GENERATED - DO NOT EDIT MANUALLY
 		bool m_mass_source = 0;
 		scalar m_mass_constant;
@@ -369,7 +401,7 @@ namespace openballistics::projectile
 		scalar m_magnus_force_coefficient_table_min;
 		scalar m_magnus_force_coefficient_table_max;
 #pragma endregion "CODEGEN PROJ-R-MEM"
-    };
+	};
 }
 
 #endif // OPENBALLISTICS_PROJECTILE_REALISTIC_HPP

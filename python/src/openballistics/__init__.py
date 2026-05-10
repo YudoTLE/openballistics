4d579e0ac6fde4e33294a324de4556297954f0e0
YudoTLE <yudojago@gmail.com>
2026-05-10 22:16:32 +0700

    chore: add __version__ to package init

--- python/src/openballistics/__init__.py
+++ python/src/openballistics/__init__.py
@@ -1,9 +1,13 @@
+from importlib.metadata import version
+
 from .ballistics import Ballistics
 from .integrator import RK4, RK45
 from .environment import Environment
 from .projectile import Projectile
 from .types import Angles, Interpolator, Table
 from .enums import Priority
+
+__version__ = version("openballistics")
 
 __all__ = [
     "Ballistics",
